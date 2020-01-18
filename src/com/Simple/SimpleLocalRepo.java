6c9abb574f10c18c52321653077f80eb0c2ddbc7
qa-amol2020 <qa.amolbegineer@gmail.com>
2020-01-18 15:10:44 +0530

    first commit...

---
+++ src/com/Simple/SimpleLocalRepo.java
@@ -0,0 +1,10 @@
+package com.Simple;
+
+public class SimpleLocalRepo {
+
+	public static void main(String[] args) {
+System.out.println("Helloo");
+System.out.println("Welcome to Local Repo");
+	}
+
+}
